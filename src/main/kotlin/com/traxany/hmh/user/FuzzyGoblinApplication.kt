d3f4a3dad06d87d966b617a69f9e79443345e24f
traxany <traxany@gmail.com>
2025-01-10 16:38:26 +0900

    Initialize hmh-user server with Spring boot

---
+++ src/main/kotlin/com/traxany/hmh/user/FuzzyGoblinApplication.kt
@@ -0,0 +1,11 @@
+package com.traxany.hmh.user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FuzzyGoblinApplication
+
+fun main(args: Array<String>) {
+    runApplication<FuzzyGoblinApplication>(*args)
+}
---
+++ src/test/kotlin/com/traxany/hmh/user/FuzzyGoblinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.traxany.hmh.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FuzzyGoblinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
